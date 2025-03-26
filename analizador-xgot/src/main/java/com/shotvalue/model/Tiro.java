feac91ad56f0a409d9197817725c0012b6b91b38
SantiagoOcaranza_ExamenGit <221B5077@uem.live.es>
2025-03-26 20:40:02 +0100

    Añado clase Tiro

---
+++ analizador-xgot/src/main/java/com/shotvalue/model/Tiro.java
@@ -0,0 +1,19 @@
+package com.shotvalue.model;
+
+public class Tiro {
+    private int x;
+    private int y;
+
+    public Tiro(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
