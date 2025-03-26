4bc597d744007d897268d2422f85864173ffe116
NinoPuma <apuma2002@gmail.com>
2025-03-26 20:08:28 +0100

    Estructura inicial del proyecto

---
+++ analizador-xgot/src/test/java/com/shotvalue/analizador_xgot/AnalizadorXgotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotvalue.analizador_xgot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalizadorXgotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
