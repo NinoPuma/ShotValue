2711886af5a9ef1fc09f98ec2ff34d5103b697a9
XxruahuxX <148128607+XxruahuxX@users.noreply.github.com>
2025-05-05 19:58:17 +0200

    Front definitivo

---
+++ analizador-xgot/src/main/java/com/shotvalue/front/AnalisisEventosController.java
@@ -0,0 +1,4 @@
+package com.shotvalue.front;
+
+public class AnalisisEventosController {
+}
---
+++ analizador-xgot/src/main/java/com/shotvalue/front/AnalizadorXgotApplication.java
@@ -0,0 +1,13 @@
+package com.shotvalue.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalizadorXgotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalizadorXgotApplication.class, args);
+	}
+
+}
